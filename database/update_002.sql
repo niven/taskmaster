60b7b26450f6fdfa2b248bb377ec2cc61c2f58ec
Chris Veenboer <git@interdictor.org>
2019-02-10 16:27:45 +0100

    Only display empty all text when neither daily nor weekly

--- database/update_002.sql
+++ database/update_002.sql
@@ -6,6 +6,6 @@ ALTER TABLE tasks ADD CONSTRAINT "tasks_domain_id_ref_domains_id_fkey_del_cascad
 ALTER TABLE minion_domain DROP CONSTRAINT "minion_domain_domain_id_ref_domains_id_fkey";
 ALTER TABLE minion_domain ADD CONSTRAINT "minion_domain_domain_id_ref_domains_id_fkey_del_cascade" FOREIGN KEY (domain_id) REFERENCES domains ON DELETE CASCADE;
 -- If tasks are dropped, it should also drop assignments
-ALTER TABLE task_assignments DROP CONSTRAINT "task_assignment_task_id_ref_task_id_fkey";
+ALTER TABLE task_assignments DROP CONSTRAINT "task_assignment_task_id_ref_tasks_id_fkey";
 ALTER TABLE task_assignments ADD CONSTRAINT "task_assignment_task_id_ref_tasks_id_fkey_del_cascade" FOREIGN KEY (task_id) REFERENCES tasks ON DELETE CASCADE;
 INSERT INTO version (point) VALUES (2);
